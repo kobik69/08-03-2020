d2b6d4125a3099888bf13cbe95d261ad0b84d565
kobi kahila <kobik69@gmail.com>
2020-03-08 23:54:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08_03_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[11];
+            arr[0] = 101;
+            arr[1] = 202;
+            arr[2] = 303;
+            arr[3] = 404;
+            arr[10] = 999;
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine("you have this value :" +item);
+                Console.WriteLine(arr[10]);
+            }
+            Console.ReadKey();       
+        }
+    }
+}
